e8d246a0d6a286f9bf1a1c081180d6f7735781a6
aidanbeck <aidanmbeck@outlook.com>
2025-08-13 17:23:43 -0500

    assignment complete

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
- class Main {
+import java.util.Scanner;
+
+class Main {
     public static void main (String[] Args) {
-        System.out.println("test");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your age: "); //not println because it shouldn't end the line.
+        int age = scanner.nextInt();
+        int yearsUntil100 = 100 - age;
+        if (yearsUntil100 <= 0) {
+            System.out.println("You are already over 100.");
+        } else if (yearsUntil100 == 1) {
+            System.out.println("You will be 100 next year.");
+        } else {
+            System.out.println("You will be 100 in " + yearsUntil100 + " years.");
+        }
+
     }
 }
